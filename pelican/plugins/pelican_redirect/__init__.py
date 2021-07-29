249019e76197bbb58bbd1f2ddba1bc916bfdbe9c
Bryan Weber <bryan.w.weber@gmail.com>
2021-07-29 14:45:41 -0400

    Write the redirect template
    If a key named original_url is in the file metadata, a redirecting file
    is written to that path in the output. The redirecting file redirects
    to the new location automatically using http-equiv.

--- pelican/plugins/pelican_redirect/__init__.py
+++ pelican/plugins/pelican_redirect/__init__.py
@@ -1,18 +1,66 @@
-# from .pelican_redirect import *  # NOQA
+import logging
+
 from pelican import signals
 from pelican.generators import CachingGenerator
 
+logger = logging.getLogger(__name__)
+
+
+TEMPLATE = """\
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="canonical" href="/{{ page.save_as }}" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="refresh" content="0;url=/{{ page.save_as }}" />
+</head>
+
+<body>
+<p>This content has moved. If you are not redirected, please click here:</p>
+<p><a href="/{{ page.save_as }}">{{ page.save_as }}</a></p>
+</body>
+
+</html>
+"""
+
 
 class RedirectGenerator(CachingGenerator):
-    def generate_context(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.redirects = []
+        signals.article_generator_finalized.connect(self.redirect_articles)
+        signals.page_generator_finalized.connect(self.redirect_pages)
+        self._templates["redirect"] = self.env.from_string(TEMPLATE)
 
-    def generate_output(self):
-        pass
+    def redirect_articles(self, article_generator):
+        for article in article_generator.articles:
+            if article.metadata.get("original_url"):
+                self.redirects.append(article)
+
+    def redirect_pages(self, page_generator):
+        for page in page_generator.pages:
+            if page.metadata.get("original_url"):
+                self.redirects.append(page)
+
+    def generate_output(self, writer):
+        for page in self.redirects:
+            logger.debug(
+                "\nSource Path: %s\nRedirect File: %s",
+                page.source_path,
+                page.metadata["original_url"],
+            )
+            writer.write_file(
+                page.metadata["original_url"],
+                self.get_template("redirect"),
+                self.context,
+                self.settings["RELATIVE_URLS"],
+                paginated=False,
+                page=page,
+            )
 
 
 def get_generators(pelican_object):
-    # define a new generator here if you need to
     return RedirectGenerator
 
 
