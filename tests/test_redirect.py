0cda1fd22e64ed0c78428bc020ebb816f13004db
Chris Rose <offline@offby1.net>
2025-05-01 17:26:51 -0700

    Add some rudimentary tests for the redirector

--- tests/test_redirect.py
+++ tests/test_redirect.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from pelican.contents import Page
 
 
 def test_generators():
@@ -32,19 +33,26 @@ def get_generator(settings, tmp_path: Path):
     return RedirectGenerator(context, settings, path, theme, output_path)
 
 
-@pytest.fixture(name="output_file")
-def get_output_file(tmp_path: Path):
+@pytest.fixture(name="output_dir")
+def get_output_dir(tmp_path: Path):
     """Fixture to get the output file"""
-    return tmp_path / "output_file.html"
+    return tmp_path / "output"
 
 
 @pytest.fixture(name="writer")
-def get_writer(settings, output_file: Path):
+def get_writer(settings, output_dir: Path):
     """Fixture to get a writer instance"""
     from pelican.writers import Writer
 
     settings["TEMPLATE_PAGES"] = {}
-    return Writer(output_file, settings)
+    return Writer(output_dir, settings)
+
+
+@pytest.fixture(name="page")
+def get_page(settings):
+    page = Page("content", {}, settings, "source-path.html.j2")
+    page.override_save_as = "dest-path.html"
+    return page
 
 
 def test_plugin_version(generator):
@@ -52,6 +60,31 @@ def test_plugin_version(generator):
     assert isinstance(generator.version, str)
 
 
-def test_plugin_version_meta(generator, writer, output_file: Path):
+def test_plugin_version_meta(generator, writer, output_dir: Path):
+    from pelican.plugins.pelican_redirect import Redirect
+
+    generator.redirects = [Redirect(to="new-url", from_url="old-path.html")]
     generator.generate_output(writer)
-    assert output_file.read_text() == "foo: bar"
+    redirect_content = (output_dir / "old-path.html").read_text()
+    assert "pelican-redirect-plugin-version" in redirect_content
+
+
+def test_plugin_redirect_to_new(generator, writer, output_dir: Path):
+    from pelican.plugins.pelican_redirect import Redirect
+
+    generator.redirects = [
+        Redirect(to="doesn't matter, ignored in this", from_url="old-path.html")
+    ]
+    generator.generate_output(writer)
+    redirect_content = (output_dir / "old-path.html").read_text()
+    assert 'content="0;url=/"' in redirect_content
+
+
+def test_plugin_redirect_to_existing(generator, writer, output_dir: Path, page: Page):
+    from pelican.plugins.pelican_redirect import Redirect
+
+    generator.redirects = [Redirect(to=page, from_url="old-path.html")]
+    generator.generate_output(writer)
+
+    redirect_content = (output_dir / "old-path.html").read_text()
+    assert f'content="0;url=/{page.save_as}"' in redirect_content
